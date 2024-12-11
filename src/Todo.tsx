52ca5ea688b5c09ca71b5aee613025a97cf80be6
Pehnny <theo.piron.pro@gmail.com>
2024-12-11 13:29:12 +0100

    Added handler for form Done - Added handler for checkbox WIP

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -3,84 +3,103 @@ import {useRef, useState} from 'react'
 // import viteLogo from '/vite.svg'
 import './Todo.css'
 
-type Task = {id: number, name: string, done: boolean};
-// type CheckTaskFunction = (id: number) => void;
+type Task = { id: number, name: string, done: boolean };
+type NewTask = (name: string) => void;
 
 export function Todo() {
-    return (
-        <>
-            <h1 className="title">Todo App</h1>
-            <TodoForm/>
-            <TodoList/>
-        </>
-    );
-}
-
-function TodoForm() {
-    const inputRef = useRef<HTMLInputElement>(null);
-
-    function handleSubmit() {
-        const inputTask = inputRef.current;
-        if (!inputRef.current) {
-            console.log("Input Element Not Found");
-            return;
-        }
-        console.log(inputTask);
-    }
-
-    return (
-        <form className="task" onSubmit={handleSubmit}>
-            <input type="text"
-                name="newTask"
-                placeholder="Entrez une nouvelle tâche"
-                ref={inputRef}/>
-            <button type="submit">Ajouter tâche</button>
-        </form>
-    );
-}
-
-function TodoList() {
-    const initialTasks = [
+    const initTasks = [
         {
             id: 1,
-            name: "Apprendre React",
+            name: "Learn React",
             done: false
         },
         {
             id: 2,
-            name: "S'amuser",
-            done: false
-        },
-        {
-            id: 3,
-            name: "Ajouter des événements",
+            name: "Make this button to work",
             done: false
         }
-    ];
+    ]
+    const [tasks, setTasks] = useState<Task[]>(initTasks);
 
-    const [tasks, setTask] = useState<Task[]>(initialTasks);
+    function handleNewTask(name: string) {
+        const newTask: Task = {id: tasks.length + 1, name, done: false};
+        setTasks([...tasks, newTask]);
+    }
 
-    function handleCheck(id: number) {
-        const newTasks = tasks.map(task => {
-            if (task.id === id) {
+    // TODO use same logic to update status from up here
+
+    return (
+        <>
+            <h1 className="title">Todo App</h1>
+            <TodoForm handleNewTask={handleNewTask}/>
+            <TodoList taskList={tasks}/>
+        </>
+    );
+}
+
+function TodoForm({handleNewTask}: { handleNewTask: NewTask }) {
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    function handleSubmit() {
+        if (!inputRef.current?.value) {
+            return;
+        }
+
+        const newTask = inputRef.current.value.trim();
+
+        if (!newTask) {
+            return;
+        }
+
+        handleNewTask(newTask);
+        inputRef.current.value = "";
+    }
+
+    return (
+        <section className="task">
+            <input type="text"
+                   name="newTask"
+                   placeholder="Entrez une nouvelle tâche"
+                   ref={inputRef}/>
+            <button type="button" onClick={handleSubmit}>Ajouter tâche</button>
+        </section>
+    );
+}
+
+function TodoList({taskList}: { taskList: Task[] }) {
+    // FIXME add event trigger on added tasks
+    const [tasks, setTaskState] = useState<Task[]>(taskList);
+
+    function handleCheck(update: Task) {
+        console.log(tasks);
+        if (tasks.length < taskList.length) {
+            console.log("hello");
+            setTaskState([...taskList]);
+        }
+        console.log(tasks);
+
+        const updatedTasks = tasks.map(task => {
+            if (task === update) {
                 task.done = !task.done;
             }
             return task;
         });
-        setTask(newTasks);
+
+        setTaskState(updatedTasks);
+        console.log(...tasks);
     }
 
     return (
         <section className="task-list">
             <h2>Tâches</h2>
             <ul>
-                {tasks.map((task) => (
+                {taskList.map((task) => (
                     <li key={task.id}>
                         <input type="checkbox"
                                id={task.id.toString()}
                                name={task.name}
                                checked={task.done}
-                               onChange={() => handleCheck(task.id)}/>
+                               onChange={() => handleCheck(task)}/>
                         <label htmlFor={task.name}>{task.name}</label>
                     </li>
                 ))}
