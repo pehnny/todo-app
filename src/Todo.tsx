b1ecceca178e7fa4e05c4c3f0c84f83f5913e4aa
Pehnny <theo.piron.pro@gmail.com>
2024-12-11 17:34:16 +0100

    Added Local Storage support

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,34 +1,27 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './Todo.css'
 import {TodoList} from "./components/TodoList.tsx";
 import {TodoForm} from "./components/TodoForm.tsx";
 import {Task} from "./types/types.tsx";
+import {loadLocalStorageOngoingTasks} from "./localStorage/loadLocalStorageOngoingTasks.tsx";
+import {saveLocalStorageData} from "./localStorage/saveLocalStorageData.tsx";
+import {loadLocalStorageID} from "./localStorage/loadLocalStorageID.tsx";
 
 export function Todo() {
-    const initTasks = [
-        {
-            id: 1,
-            name: "Learn React",
-            done: false
-        },
-        {
-            id: 2,
-            name: "Make this button to work",
-            done: true
-        },
-        {
-            id: 3,
-            name: "Make these checkboxes to work",
-            done: true
-        }
-    ]
-    const [tasks, setTasks] = useState<Task[]>(initTasks);
+    const [tasks, setTasks] = useState<Task[]>(loadLocalStorageOngoingTasks);
+
+    const maxID = Math.max(...tasks.map(task => task.id), 0);
+    const [id, setId] = useState(() => loadLocalStorageID(maxID));
+
+    useEffect(() => saveLocalStorageData(tasks, id), [tasks, id]);
 
     function handleNewTask(name: string) {
-        const newTask: Task = {id: tasks.length + 1, name, done: false};
+        const newId = id + 1;
+        const newTask: Task = {id: newId, name, done: false};
 
+        setId(newId);
         setTasks([...tasks, newTask]);
     }
 
@@ -37,6 +30,7 @@ export function Todo() {
             if (task.id === id) {
                 task.done = !task.done;
             }
+
             return task;
         });
 
@@ -52,6 +46,3 @@ export function Todo() {
         </>
     );
 }
-
-
-
