25c2ab6f1ef914ca64b563441ad92d13f9b4360b
Pehnny <theo.piron.pro@gmail.com>
2024-12-13 12:20:32 +0100

    Added types for the new features

--- src/types/types.tsx
+++ src/types/types.tsx
@@ -1,3 +1,4 @@
 export type Task = { id: number, name: string, done: boolean };
 export type NewTask = (name: string) => void;
-export type UpdateTask = (id: number) => void;
+export type ActionTask = (id: number) => void;
+export type ActionTasks = () => void;
