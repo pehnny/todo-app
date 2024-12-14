53a14f466f6b3d08ef458763da5ff884ef6fb256
Pehnny <theo.piron.pro@gmail.com>
2024-12-14 20:00:04 +0100

    Remove unused local storage keys

--- src/localStorage/saveLocalStorageData.tsx
+++ src/localStorage/saveLocalStorageData.tsx
@@ -2,6 +2,6 @@ import {LocalStorageKeys} from "./LocalStorageKeys.tsx";
 import {Task} from "../types/types.tsx";
 
 export function saveLocalStorageData(tasks: Task[], id: number) {
-    localStorage.setItem(LocalStorageKeys.Ongoing, JSON.stringify(tasks));
+    localStorage.setItem(LocalStorageKeys.Tasks, JSON.stringify(tasks));
     localStorage.setItem(LocalStorageKeys.ID, JSON.stringify(id));
 }
