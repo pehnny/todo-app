deba7815a66f73df6f921acb0c76e85505eea6e6
Pehnny <theo.piron.pro@gmail.com>
2024-12-14 20:02:49 +0100

    Added ongoing tasks counter

--- src/components/TodoCounter.tsx
+++ src/components/TodoCounter.tsx
@@ -1,6 +1,6 @@
 export function TodoCounter({taskCount}: { taskCount: number }) {
     return (
-        <section>
+        <section className="task-counter">
             <h2>Tâches en cours</h2>
             <p>{taskCount}</p>
         </section>
