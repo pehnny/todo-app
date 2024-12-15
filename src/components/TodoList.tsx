55456f520a4eb7c24281c3bd5c93e277add2e04a
Pehnny <theo.piron.pro@gmail.com>
2024-12-15 16:55:36 +0100

    Added style responsiveness for task-list-type

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -10,25 +10,28 @@ export function TodoList({taskList, handleUpdateTask, handleDeleteTask, handleDe
     return (
         <section className="task-list">
             <h2>Tâches</h2>
-            {Object.values(TaskType).map((taskType: TaskType) => (
-                <ul key={taskType} className={`task-list-${taskType}`}>
-                    <h3>{taskType}</h3>
-                    {taskList
-                        .filter((task: Task) => (task.type === taskType))
-                        .map((task: Task) => (
-                            <li key={task.id}>
-                                <input type="checkbox"
-                                       id={task.id.toString()}
-                                       name={task.name}
-                                       checked={task.done}
-                                       onChange={() => handleUpdateTask(task.id)}/>
-                                <label htmlFor={task.name}>{task.name}</label>
-                                <button type="button" onClick={() => handleDeleteTask(task.id)}>X</button>
-                            </li>
-                        ))
-                    }
-                </ul>
-            ))}
+            <section className="task-list-type"
+                     style={{gridTemplateColumns: `repeat(${Object.values(TaskType).length}, 1fr)`}}>
+                {Object.values(TaskType).map((taskType: TaskType) => (
+                    <ul key={taskType} className={`task-list-type-${taskType}`}>
+                        <h3>{taskType}</h3>
+                        {taskList
+                            .filter((task: Task) => (task.type === taskType))
+                            .map((task: Task) => (
+                                <li key={task.id}>
+                                    <label htmlFor={`${task.id}-${task.name}`}>{task.name}</label>
+                                    <input type="checkbox"
+                                           id={task.id.toString()}
+                                           name={`${task.id}-${task.name}`}
+                                           checked={task.done}
+                                           onChange={() => handleUpdateTask(task.id)}/>
+                                    <button type="button" onClick={() => handleDeleteTask(task.id)}>x</button>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                ))}
+            </section>
             <button onClick={handleDeleteDoneTasks}>Retirer les tâches complétées</button>
         </section>
     );
